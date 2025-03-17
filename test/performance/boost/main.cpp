bb7120f0603101e216e78ad77b10389e1d63a98b
kalashnikovni <denise.marzorati@gmail.com>
2025-03-17 17:03:11 -0300

    [iss-44]

--- test/performance/boost/main.cpp
+++ test/performance/boost/main.cpp
@@ -144,7 +144,7 @@ void parseEvalProgramFromFile(int alg, std::string fname, int copies)
   if (r && iter == end) {
     SBG::LIB::UnordAF set_fact;
     SBG::LIB::MapAF map_fact(set_fact);
-    SBG::LIB::UnordPWMapAF fact(map_fact);
+    auto fact = std::make_shared<SBG::LIB::UnordPWMapAF>(map_fact);
     SBG::Eval::ProgramVisitor program_visit(fact, false); 
     SBG::Eval::ProgramIO visit_result = boost::apply_visitor(
       program_visit, parser_result

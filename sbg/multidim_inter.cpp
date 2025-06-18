9657d09b9b1559a9e787067ce16f8fd1623aca42
kalashnikovni <denise.marzorati@gmail.com>
2025-06-18 12:12:32 -0300

    Debugged MDI compact

--- sbg/multidim_inter.cpp
+++ sbg/multidim_inter.cpp
@@ -17,6 +17,7 @@
 
  ******************************************************************************/
 
+#include <iostream>
 #include "sbg/multidim_inter.hpp"
 
 namespace SBG {
@@ -185,17 +186,19 @@ MaybeMDI MultiDimInter::compact(const MultiDimInter &other) const
   unsigned int j = 0;
   for (; j < arity(); ++j) {
     auto ith = operator[](j).compact(other[j]);
-    if (ith) {
-      res.emplaceBack(ith.value());
-      ++j;
-      break;
-    }
-
-    else if (operator[](j) != other[j])
-      return {};
-
-    else
+    if (operator[](j) == other[j])
       res.emplaceBack(operator[](j));
+
+    else {
+      if (ith) {
+        res.emplaceBack(ith.value());
+        ++j;
+        break;
+      }
+
+      else
+        return {};
+    }
   }
 
   for (; j < arity(); ++j) {

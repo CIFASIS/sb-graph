d964af01d5725f7e40a40c10baf5608a309566fd
Franco Sansone <sansone@cifasis-conicet.gov.ar>
2025-08-25 13:32:52 -0300

    select if using multiple intial paritition strategies by a flag

--- algorithms/partitioner/partition_graph.cpp
+++ algorithms/partitioner/partition_graph.cpp
@@ -30,7 +30,6 @@
 #include "partition_graph.hpp"
 #include "sbg_partitioner_log.hpp"
 
-#define TRY_MULTIPLE_STRATEGIES 1
 
 using namespace std;
 
@@ -68,48 +67,34 @@ Set get_communication_edges(Set partition, const PWMap& map_1, const PWMap& map_
   return size;
 }
 
-constexpr bool using_many_initial_partitions = TRY_MULTIPLE_STRATEGIES;
-}  // namespace
 
-
-// we could cache solutions here
-Set from_vector(const Partition& partition, const SetAF& set_fact) {
-    Set partition_set = set_fact.createSet();
-    for (size_t i = 0; i < partition.size(); i++) {
-        partition_set.emplace(partition[i]);
-    }
-
-    return partition_set;
-}
-
-
-Partition to_vector(const Set& partition_set)
-{
-    Partition partition;
-    for (auto set_piece : partition_set) {
-        partition.push_back(move(set_piece));
-    }
-
-    return partition;
-}
-
-
-vector<PartitionMap> make_initial_partitions(SBG::LIB::WeightedSBGraph& graph, unsigned number_of_partitions)
+vector<PartitionMap> make_initial_partitions(SBG::LIB::WeightedSBGraph& graph, unsigned number_of_partitions,
+    const InitialPartitionStrategy strategy)
 {
   vector<PartitionMap> partitions_sets;
   initialize_partitioning(graph, number_of_partitions);
 
   constexpr bool pre_order = true;
-  auto s1 = PartitionStrategyDistributive(number_of_partitions, graph);
-  add_strategy(s1, pre_order);
-#if TRY_MULTIPLE_STRATEGIES
-  auto s2 = PartitionStrategyDistributive(number_of_partitions, graph);
-  add_strategy(s2, not pre_order);
-  auto s3 = PartitionStrategyGreedy(number_of_partitions, graph);
-  add_strategy(s3, pre_order);
-  auto s4 = PartitionStrategyGreedy(number_of_partitions, graph);
-  add_strategy(s4, not pre_order);
-#endif
+  const bool all_strategies = strategy == InitialPartitionStrategy::ALL;
+  if (all_strategies or strategy == InitialPartitionStrategy::DFS_DISTRIBUTIVE_PREORDER) {
+    auto s1 = make_unique<PartitionStrategyDistributive>(number_of_partitions, graph);
+    add_strategy(move(s1), pre_order);
+  }
+
+  if (all_strategies or strategy == InitialPartitionStrategy::DFS_DISTRIBUTIVE_POSTORDER) {
+    auto s2 = make_unique<PartitionStrategyDistributive>(number_of_partitions, graph);
+    add_strategy(move(s2), not pre_order);
+  }
+
+  if (all_strategies or strategy == InitialPartitionStrategy::DFS_GREEDY_PREORDER) {
+    auto s3 = make_unique<PartitionStrategyGreedy>(number_of_partitions, graph);
+    add_strategy(move(s3), pre_order);
+  }
+
+  if (all_strategies or strategy == InitialPartitionStrategy::DFS_GREEDY_POSTORDER) {
+    auto s4 = make_unique<PartitionStrategyGreedy>(number_of_partitions, graph);
+    add_strategy(move(s4), not pre_order);
+  }
 
   vector<map<unsigned, set<SetPiece>>> partitions = partitionate();
 
@@ -144,13 +129,40 @@ vector<PartitionMap> make_initial_partitions(SBG::LIB::WeightedSBGraph& graph, u
   return partitions_sets;
 }
 
-PartitionMap best_initial_partition(WeightedSBGraph& graph, unsigned number_of_partitions)
+}  // namespace
+
+
+// we could cache solutions here
+Set from_vector(const Partition& partition, const SetAF& set_fact) {
+    Set partition_set = set_fact.createSet();
+    for (size_t i = 0; i < partition.size(); i++) {
+        partition_set.emplace(partition[i]);
+    }
+
+    return partition_set;
+}
+
+
+Partition to_vector(const Set& partition_set)
 {
-  std::vector<sbg_partitioner::PartitionMap> partition_maps = make_initial_partitions(graph, number_of_partitions);
+    Partition partition;
+    for (auto set_piece : partition_set) {
+        partition.push_back(move(set_piece));
+    }
+
+    return partition;
+}
+
+
+PartitionMap best_initial_partition(WeightedSBGraph& graph, unsigned number_of_partitions,
+    const InitialPartitionStrategy strategy)
+{
+  logging::sbg_log << "computing strategy number " << strategy << endl;
+  std::vector<sbg_partitioner::PartitionMap> partition_maps = make_initial_partitions(graph, number_of_partitions, strategy);
 
   auto& best_initial_partitions = partition_maps.front();
   CommunicationCost comm_cost = CommunicationCost(graph, best_initial_partitions);
-  if (using_many_initial_partitions) {
+  if (strategy == InitialPartitionStrategy::ALL) {
     size_t best_communication_set_cardinality = get_partition_communication(graph, best_initial_partitions);
     for (unsigned i = 0; i < number_of_partitions; i++) {
         best_communication_set_cardinality += get_set_size(comm_cost.get_ec_by_partition_id(i));
--- test/partitioner/partitioner_test.cpp
+++ test/partitioner/partitioner_test.cpp
@@ -121,7 +121,7 @@ TEST(initial_partition, PartitionerTests)
 
   auto sb_graph = sbg_partitioner::build_sb_graph(get_full_file_name("air_conditioners_1000.json"), pw_fact);
 
-  sbg_partitioner::PartitionMap partition = sbg_partitioner::best_initial_partition(sb_graph, 4);
+  sbg_partitioner::PartitionMap partition = sbg_partitioner::best_initial_partition(sb_graph, 4, sbg_partitioner::InitialPartitionStrategy::ALL);
 
   auto expected_distributed_pre_order_0 = set_fact.createSet();
   expected_distributed_pre_order_0.emplaceBack(Interval(0, 1, 249));
@@ -163,7 +163,7 @@ static void test_partitioning(const std::string& filename, int number_of_partiti
   UnordPWMapAF pw_fact(map_fact);
 
   auto sb_graph = sbg_partitioner::build_sb_graph(filename, pw_fact);
-  auto partitions = sbg_partitioner::best_initial_partition(sb_graph, number_of_partitions);
+  auto partitions = sbg_partitioner::best_initial_partition(sb_graph, number_of_partitions, sbg_partitioner::InitialPartitionStrategy::ALL);
   sbg_partitioner::kl_sbg_imbalance_partitioner(sb_graph, partitions, 0.0);
 
   sbg_partitioner::sanity_check(sb_graph, partitions, number_of_partitions);

50773b829980a4bb3ee1f5ae9142e2cb97c1d5c3
kalashnikovni <denise.marzorati@gmail.com>
2025-12-30 07:42:38 -0300

    Updated performance scripts

--- test/performance/scc/perf_scc.sh
+++ test/performance/scc/perf_scc.sh
@@ -12,7 +12,7 @@
 #                will be the average of all runs.
 #                [SIZE] Sets the size of variable N in the test to SIZE.
 #       OPTIONS: ---
-#  REQUIREMENTS: [1] ../../../bin/sbg-eval binary. 
+#  REQUIREMENTS: [1] ../../../build/eval/sbg-eval binary. 
 #         NOTES: --- 
 #        AUTHOR: Denise Marzorati, denise.marzorati@gmail.com
 #       PROJECT: Set Based Graph Library
@@ -33,7 +33,7 @@ echo > builder_values
 
 while read line; do
     if  echo "$line" | grep -q "N = "; then 
-        echo $line > eval; awk -v s=$size '{print "N = " s}' eval >> test_file;
+        echo $line > eval; awk -v s=$size '{print "N = " s ";"}' eval >> test_file;
     else
         echo $line >> test_file
     fi
@@ -41,11 +41,11 @@ done < aux_file
 
 for i in $(seq "$iterations"); do
 	echo $i
-	../../../bin/sbg-eval -f test_file
+	../../../build/eval/sbg-eval test_file -p 1
 	mv SBG.log SBG_${i}.log
 	echo
     while read line; do
-        if  echo "$line" | grep -q "Total SCC"; then echo $line > eval; awk '{print $5}' eval >> test_values; fi
+        if  echo "$line" | grep -q "Total MinReachSCC"; then echo $line > eval; awk '{print $5}' eval >> test_values; fi
         if  echo "$line" | grep -q "SBG SCC builder"; then echo $line > eval; awk '{print $4}' eval >> builder_values; fi
     done < "SBG_${i}.log"
 done

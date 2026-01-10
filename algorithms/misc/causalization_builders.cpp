25eb360ab80d9ef71b483780687b06b894bcdf88
kalashnikovni <denise.marzorati@gmail.com>
2026-01-10 17:46:50 -0300

    Use bipartite SBG in builders

--- algorithms/misc/causalization_builders.cpp
+++ algorithms/misc/causalization_builders.cpp
@@ -36,12 +36,23 @@ SBG::LIB::DSBG buildSCCFromMatching(const SBG::LIB::MatchData& data)
   SBG::LIB::Set V = M.compact();
   SBG::LIB::PWMap auxVmap = data.bsbg().subEmap().restrict(M);
   SBG::LIB::PWMap Vmap = SBG::LIB::PW_FACT.createPWMap();
-  for (const SBG::LIB::Map& map : auxVmap) 
+  for (const SBG::LIB::Map& map : auxVmap) { 
     Vmap.emplaceBack(SBG::LIB::Map(map.dom().compact()
       , map.exp()));
+  }
 
-  SBG::LIB::PWMap mapF = bsbg.map1();
-  SBG::LIB::PWMap mapU = bsbg.map2();
+  SBG::LIB::PWMap map1 = bsbg.map1();
+  SBG::LIB::PWMap map2 = bsbg.map2();
+
+  SBG::LIB::Set X = bsbg.X();
+  SBG::LIB::PWMap map1_toX = map1.restrict(map1.preImage(X));
+  SBG::LIB::PWMap map2_toX = map2.restrict(map2.preImage(X));
+  SBG::LIB::PWMap mapF = map1_toX.concatenation(map2_toX);
+
+  SBG::LIB::Set Y = bsbg.Y();
+  SBG::LIB::PWMap map1_toY = map1.restrict(map1.preImage(Y));
+  SBG::LIB::PWMap map2_toY = map2.restrict(map2.preImage(Y));
+  SBG::LIB::PWMap mapU = map1_toY.concatenation(map2_toY);
 
   SBG::LIB::PWMap matchedF_inv = mapF.restrict(M).inverse();
   SBG::LIB::PWMap unmatchedF = mapF.restrict(free_edges);

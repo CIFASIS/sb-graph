d7f311bc319f805d9e40c62f10995580529a6a79
Yamil Saer <yamil.saer@gmail.com>
2026-02-08 13:24:40 -0300

    - Added tearing function to parser and evaluator

--- eval/visitors/expr_evaluator.cpp
+++ eval/visitors/expr_evaluator.cpp
@@ -66,6 +66,8 @@ ExprEvaluator::ExprEvaluator(EvalContext& eval_ctx) : _eval_context(eval_ctx)
   eval_ctx.insertFunction("match", BuiltInFunctions::matchingEvaluator);
   eval_ctx.insertFunction("scc", BuiltInFunctions::sccEvaluator);
   eval_ctx.insertFunction("matchSCC", BuiltInFunctions::matchSCCEvaluator);
+  eval_ctx.insertFunction("tearing", BuiltInFunctions::tearingEvaluator);
+  eval_ctx.insertFunction("matchTearing", BuiltInFunctions::matchTearingEvaluator);
   eval_ctx.insertFunction("mfvs", BuiltInFunctions::mfvsEvaluator);
   eval_ctx.insertFunction("matchSCCMFVS"
     , BuiltInFunctions::matchSCCMFVSEvaluator);
--- eval/visitors/func_evaluator.hpp
+++ eval/visitors/func_evaluator.hpp
@@ -98,6 +98,10 @@ public:
   static ExprBaseType mfvsEvaluator(const EBTList& args);
   static ExprBaseType matchSCCMFVSEvaluator(const EBTList& args);
   static ExprBaseType topoSortEvaluator(const EBTList& args);
+  static ExprBaseType cutVertexEvaluator(const EBTList& args);
+  static ExprBaseType matchSCCEvaluator(const EBTList& args);
+  static ExprBaseType tearingEvaluator(const EBTList& args);
+  static ExprBaseType matchTearingEvaluator(const EBTList& args);
   static ExprBaseType causalizationEvaluator(const EBTList& args);
 };
 
